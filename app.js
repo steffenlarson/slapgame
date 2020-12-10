365be8ae8dcba0fcbc44e8438a8610ce5787b651
Steffen Larson <steffman13@gmail.com>
2020-12-10 15:36:37 -0700

    functionality done?

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 let health = 100
+let victim = "Jar Jar"
+let hits = 0
 
 function slap() {
     health = health - 1
     console.log(health)
     update()
+    update2()
 }
 
-function update() {
-    let healthUpdate = document.getElementById("health")
-    healthUpdate.innerText = `${health} You have been slapped!`
-}
 
 function punch() {
     health = health - 5
     console.log(health)
     update()
+    update2()
 }
 
 function kick() {
     health = health - 10
     console.log(health)
     update()
+    update2()
 }
+
+function update() {
+    let healthUpdate = document.getElementById("health")
+    healthUpdate.innerText = `${health} Hit Him Again!`
+
+
+
+}
+
+function update2() {
+    let hitsUpdate = document.getElementById("tag")
+    hitsUpdate.innerText = `${hits} OWCH!`
+}
+
+function hitCounter() {
+    hits = hits + 1
+}
+
+
