33a0c4fa14c3035bf851424a8a17337598c6ff03
Steffen Larson <steffman13@gmail.com>
2020-12-10 17:07:41 -0700

    latest update

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ let adversary = [
     }
 ]
 
+let playerChoice = 0
 
+function select(tree) {
 
-function select() {
-
+    playerChoice = tree
+    console.log(playerChoice)
 }
 
 // let health = 100
@@ -25,35 +27,36 @@ function select() {
 let hits = 0
 
 function slap() {
-    adversary.health = adversary.health - 1
-    console.log(adversary.health)
+    adversary[playerChoice].health = adversary[playerChoice].health - 1
+    console.log(adversary[playerChoice].health)
     update()
     update2()
 }
 
 
-function punch() {
-    adversary.[health] = adversary.health - 5
-    console.log(adversary.health)
-    update()
-    update2()
-}
+// function punch() {
+//     adversary.[health] = adversary.health - 5
+//     console.log(adversary.health)
+//     update()
+//     update2()
+// }
 
-function kick() {
-    adversary.health = adversary.health - 10
-    console.log(adversary.health)
-    update()
-    update2()
-}
+// function kick() {
+//     adversary.health = adversary.health - 10
+//     console.log(adversary.health)
+//     update()
+//     update2()
+// }
 
 function update() {
     let healthUpdate = document.getElementById("health")
-    healthUpdate.innerText = `${health} Hit Him Again!`
+    healthUpdate.innerText = `${adversary[playerChoice].health} Hit Him Again!`
+    let hitsUpdate = document.getElementById("tag")
+    hitsUpdate.innerText = `${hits} OWCH!`
+
 }
 
 function update2() {
-    let hitsUpdate = document.getElementById("tag")
-    hitsUpdate.innerText = `${hits} OWCH!`
 }
 
 function hitCounter() {
